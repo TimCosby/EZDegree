136ea4d4e6e7a3d1537ef7abaa0ea32d7ce7c835
Tim <ninjayoshi@users.noreply.github.com>
2017-04-26 22:22:48 -0400

    New system to authenticate - no encryption

---
+++ Authentication.py
@@ -0,0 +1,36 @@
+from openpyxl import load_workbook
+
+WORKBOOK = load_workbook('authentication.xlsx')
+WORKSHEET = WORKBOOK.active
+
+def authenticate(username, password, users, workbook, worksheet):
+    """
+    
+    @param str username: 
+    @param str password: 
+    @param dict of int users:
+    @param Workbook workbook:
+    @param Worksheet worksheet:
+    @return: bool
+    """
+
+    try:
+        return password == WORKSHEET.cell(column=2, row=users[username]).value
+    except KeyError:
+        if 'y' in input('Create a new user?\n').lower():
+            row = max(users.values())
+
+            users[username] = row
+
+            WORKSHEET.cell(column=1, row=row, value=str(username))
+            WORKSHEET.cell(column=2, row=row, value=str(password))
+            WORKBOOK.save('authentication.xlsx')
+
+            worksheet.cell(column=1, row=row, value=username)
+            worksheet.cell(column=2, row=row, value=str({}))
+            worksheet.cell(column=3, row=row, value=str([]))
+            workbook.save('database.xlsx')
+
+            return True
+        else:
+            return False
