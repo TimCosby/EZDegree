2c57e1b17ac5f3e9262419812217227a00f8a220
ninjayoshi <tim470773@gmail.com>
2017-05-26 12:27:24 -0400

    Reworked

--- Extra/Preload.py
+++ Extra/Preload.py
@@ -1,47 +1,118 @@
-from Course import is_same
 from ast import literal_eval
 
+TEST_PATH = '..\data\\testprograms.txt'
+PATH = '..\data\programs.txt'
+FINAL_PATH = '..\data\data.dat'
 
-def recur(requirements, special, destroy=None):
-    index = 0
-    while index < len(requirements):
-        if isinstance(requirements[index], int) or isinstance(requirements[index], float) or 'PASS' in requirements[index] or 'CREDITS' in requirements[index] or 'MARK' in requirements[index]:
+
+def is_same(string, other):
+    """
+    For abstract courses (ie. 'CSC4****')
+
+    @param str string: Abstract Course code 
+    @param str other: Another Course code
+    @return: bool
+    """
+
+    # If the two strings are the same length
+    if len(string) - 1 == len(other) if string[-1] == 'X' else len(string) == len(other):
+
+        # If both courses aren't abstract
+        if '*' not in other:
+            same = True
+            # For every index in the course code
+            for index in range(len(string) - 1 if string[-1] == 'X' else len(string)):
+                # If the index is not an abstract
+                if string[index] != '*':
+                    # If the indexes do not match
+                    if string[index] != other[index]:
+                        same = False
+                        break
+            if same:
+                return True
+    return False
+
+
+def recur(list_, match=None):
+    count = 1  # Skip the modifier
+
+    while count != len(list_):
+        if isinstance(list_[count], int) or isinstance(list_[count], float):
+            # Skips the min value if exists
             pass
 
-        elif isinstance(requirements[index], list):
-            requirements[index] = recur(requirements[index], special, destroy=destroy)
-            if len(requirements[index]) == 2:
-                requirements.pop(index)
-                index -= 1
+        elif isinstance(list_[count], list):
+            # If a nested requirement
 
-        elif destroy is not None:
-            if not is_same(destroy, requirements[index]):
-                requirements.pop(index)
-                index -= 1
+            temp = recur(list_[count], match=match)  # Go through the nested requirement
 
-        elif 'ELG' in requirements[index]:
-            requirements[index] = recur(special['ELG'][requirements[index][4:7]], special, destroy=requirements[index][8:])
-            requirements[index].append(len(requirements[index]) - 1)
+            if temp is not None:
+                list_[count] = temp
 
-        index += 1
+            else:  # If the nested requirement was empty after matching
+                list_.pop(count)
+                count -= 1
 
-    return requirements
+        elif 'ELG' in list_[count]:
+            # If a group
 
-lines = open('programs.txt').readlines()
+            temp = dict_['ELG'][list_[count][4:7]]  # Get the group code
 
-#special = literal_eval(lines[1])
+            temper = recur(temp, match=list_[count][8:])  # Go through the group, only pulling out whatever matches
 
-final = {}
+            list_.pop(count)  # Get rid of the original value
 
-for line in lines[2:]:
-    if line != '' and '@' not in line:
-        print(line)
-        final.update(literal_eval(line))
+            for i in range(len(temper)):
+                list_.insert(count, temper[i])  # Put in everything matching from the group in the requirement
+                count += 1
 
-for program in final:
-    #recur(final[program]['requirements'], special)
-    final[program]['requirements'].append(len(final[program]['requirements']) - 1)
+        elif match is not None:
+            # If needs to look for specific codes
+            if not is_same(match, list_[count]):  # If the code does not match remove it
+                list_.pop(count)
+                count -= 1
 
-from os.path import join
-from os import pardir
-open(join(pardir, 'data\\data.dat'), 'w').write(str(final))
+        count += 1
+
+    if len(list_) == 2 and (isinstance(list_[-1], int) or isinstance(list_[-1], float)):  # If just contains the modifier and limit
+        print(list_[0], 'did1')
+        return None
+
+    elif len(list_) == 3 and (isinstance(list_[-2], int) or isinstance(list_[-2], float)):  # If just contains the modifer and min&max limits
+        print(list_[0], 'did2', list_[-2])
+        return None
+
+    else:
+        return list_
+
+if 'y' in input('Test y/n\n').lower():
+    file = open(TEST_PATH).readlines()
+    test = True
+else:
+    file = open(PATH).readlines()
+    test = False
+
+dict_ = {}
+
+for line in file:
+    if line[0] == '@':  # If a comment
+        pass
+
+    else:  # If not a comment, add to the dictionary
+        dict_.update(literal_eval(line))
+
+for program in dict_:
+    if program != 'ELG':  # If not a group
+        recur(dict_[program]['requirements'])
+
+        if not test:
+            dict_[program]['requirements'].append(len(dict_[program]['requirements']) - 1)  # Add the needed amount to be passed
+
+try:
+    dict_.pop('ELG')  # Get rid of groups
+except KeyError:
+    pass
+
+new_file = open(FINAL_PATH, 'w')
+new_file.write(str(dict_))
+new_file.close()
