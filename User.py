85a3dd634c8ab13cf4688c2437d04f0ff8ca75c7
ninjayoshi <tim470773@gmail.com>
2017-05-30 09:47:50 -0400

    Bug fixes, documentation

--- User.py
+++ User.py
@@ -4,14 +4,20 @@ from ast import literal_eval
 from openpyxl import load_workbook
 from Authentication import authenticate
 from copy import deepcopy
+from copy import copy
 
 '''
 TODO:
-1. Make a file to convert ELGs
+1. PORT INTO ANOTHER LANGUAGE BECAUSE PYTHON SUCKS AT MULTITASKING
+    a) Multi-thread
+    b) Prioritize taken programs
 
-2. Expand on User
+2. Load in all the programs
+    a) Hell on earth
+    
+3. Make it so that when a new course is added all ABSTRACT courses are gone through and add it to their special list of some sort
 
-3. Load in all the programs
+4. Make it so that there is a breadth counter
 '''
 
 PROGRAM_FILE = 'data\data.dat'  #'data\\testprograms.txt'
@@ -52,10 +58,12 @@ class User:
             Whether or not the user successfully logged in
         @param Courses _courses:
             A Courses object containing all functions towards courses
-        @param dict _taken_courses:
-            A dictionary of Course objects that the user has added
-        @param dict _programs:
-            A dictionary of program codes that the user has added
+        @param list _taken_courses:
+            A list of Course objects that the user has added
+        @param list _taken_programs:
+            A list of program codes that the user has added
+        @param dict _program_requirement_cache:
+            A dictionary of Requirement objects for their respective programs
     """
     def __init__(self, username, password):
         if not authenticate(username.lower(), password, USERS, WORKBOOK, WORKSHEET):
@@ -65,90 +73,214 @@ class User:
             self.username = username.lower()
 
             self._courses = Courses()
-            self._taken_courses = {}
-            self._programs = {}
+            self._taken_courses = []
+            self._taken_programs = []
+            self._program_requirement_cache = {}
 
             self.initial_courses()
             self.initial_programs()
+            self.initial_program_cache()
 
     def logged_in(self):
+        """
+        Return if the user is logged in or not
+
+        :return: bool
+        """
+
         return self._logged_in
 
-    def get_easiest(self):
-        values = {'percentage': [], 'completed': []}
-        for program in DEFAULT_PROGRAMS:
-            temp = self.convert_to_requirement(deepcopy(DEFAULT_PROGRAMS[program]['requirements']))
+    def _update_requirements(self):
+        """
+        Update all program requirement objects
 
-            percentage = (temp.have / temp.need if temp.need != 0 else 1) * 100  # testing
-            completed = temp.need - temp.have
+        :return: None
+        """
 
-            values['percentage'].append([DEFAULT_PROGRAMS[program]['name'], percentage])
-            values['completed'].append([DEFAULT_PROGRAMS[program]['name'], completed])
+        for requirement in self._program_requirement_cache:
+            self._program_requirement_cache[requirement].update
 
-        values['percentage'].sort(key=lambda x: x[1])
-        values['completed'] = sorted(values['completed'], reverse=False, key=lambda x: x[1])
-        return values
+    # ============= ADD METHODS ================== #
 
-    def get_courses(self):
-        return self._taken_courses
+    def add_program(self, program_code):
+        """
+        Add <program_code> to the user's taken programs
 
-    def get_course(self, course_code):
-        return self._taken_courses[course_code]
+        :param str program_code: Program code to add
+        :return: None
+        """
 
-    def add_course(self, course_code):
+        if program_code not in self._taken_programs:
+            WORKSHEET.cell(column=3, row=USERS[self.username], value=str(self._taken_programs))
+            WORKBOOK.save(FILE_NAME)
+
+        else:
+            print('Program already added!')
+
+    def add_course(self, course_code, initial=False):
+        """
+        Add <course_code> to the user's taken courses
+
+        :param str course_code: Course to add
+        :param bool initial: If doing the initial startup - don't bother updating all the requirements since they aren't made yet
+        :return: None
+        """
+
         # Collective courses
         self._courses.add_course(course_code)
 
         # Local courses
-        self._taken_courses[course_code] = self._courses.course_cache[course_code]
+        self._taken_courses.append(course_code)
 
         # Database courses
-        WORKSHEET.cell(column=2, row=USERS[self.username], value=str([[course.course_code, course.type, course.mark] for course in self._taken_courses.values()]))
+        WORKSHEET.cell(column=2, row=USERS[self.username], value=str([[course_code, self._courses.get_type(course_code), self._courses.get_mark(course_code)] for course_code in self._taken_courses]))
+        WORKBOOK.save(FILE_NAME)
+        if not initial:
+            self._update_requirements()
+
+    # ============= REMOVE METHODS ================== #
+
+    def remove_program(self, program_code):
+        """
+        Remove <program_code> from user's taken programs
+
+        :param str program_code: Program to remove
+        :return: None
+        """
+
+        try:
+            self._taken_programs.remove(program_code)
+        except KeyError:
+            print('Program does not exist!')
+
+        WORKSHEET.cell(column=3, row=USERS[self.username], value=str(self._taken_programs))
         WORKBOOK.save(FILE_NAME)
 
     def remove_course(self, course_code):
+        """
+        Remove <course_code> from user's taken courses
+
+        :param str course_code: Course to remove
+        :return: None
+        """
+
         # Collective courses
         self._courses.remove_course(course_code)
 
         # Local courses
-        self._taken_courses.pop(course_code)
+        self._taken_courses.remove(course_code)
 
         # Database courses
-        WORKSHEET.cell(column=2, row=USERS[self.username], value=[[course.course_code, course.type, course.mark] for course in self._taken_courses.values()])
+        WORKSHEET.cell(column=2, row=USERS[self.username], value=str([[course_code, self._courses.get_type(course_code), self._courses.get_mark(course_code)] for course_code in self._taken_courses]))
         WORKBOOK.save(FILE_NAME)
+        self._update_requirements()
+
+    # ============= GET METHODS ================== #
+
+    def get_programs(self):
+        """
+        Return program names
+
+        :return: list of str
+        """
+
+        return self._taken_programs
+
+    def get_program(self, program_code):
+        """
+        Return <program_code>'s program information
+
+        :param str program_code: Program to get information from
+        :return: dict of str
+        """
+
+        temp = copy(DEFAULT_PROGRAMS[program_code])
+        temp.pop('requirements')
+        return temp
+
+    def get_program_requirements(self, program_code):
+        """
+        Return Requirement for <program_code>
+
+        :param str program_code: Program to get requirements for
+        :return: Requirement
+        """
+
+        return self._program_requirement_cache[program_code]
+
+    def get_courses(self):
+        """
+        Return a list of all courses the user is taking
+
+        :return: list of str
+        """
+        return self._taken_courses
+
+    def get_course(self, course_code):
+        """
+        Return <course_code>'s course object
+
+        :param str course_code: Course data is desired of
+        :return: Course
+        """
+
+        return self._courses.course_cache[course_code]
 
     def get_mark(self, course_code):
+        """
+        Return mark for <course_code>
+
+        :param str course_code: Course desired mark is in
+        :return: float
+        """
+
         return self._taken_courses[course_code].mark
 
-    def set_mark(self, course_code, mark):
-        try:
-            self._courses.change_mark(course_code, mark)
-
-            WORKSHEET.cell(column=2, row=USERS[self.username], value=str([[course.course_code, course.type, course.mark] for course in self._taken_courses.values()]))
-            WORKBOOK.save(FILE_NAME)
-        except KeyError:
-            print('Course does not exist!')
-
     def get_type(self, course_code):
+        """
+        Return type for <course_code>
+
+        :param str course_code: Course desired type is in
+        :return: str
+        """
+
         return self._taken_courses[course_code].type
 
-    def set_type(self, course_code, type):
-        try:
-            if type == 'Completed' and self._taken_courses[course_code] < 50:
-                type = 'Failed'
+    def get_easiest(self):
+        """
+        Return a list of easiest to get programs by percentage and courses needed
 
-            self._courses.change_type(course_code, type)
+        :return: dict of list
+        """
 
-            WORKSHEET.cell(column=2, row=USERS[self.username], value=str([[course.course_code, course.type, course.mark] for course in self._taken_courses.values()]))
-            WORKBOOK.save(FILE_NAME)
-        except KeyError:
-            print('Course does not exist!')
+        values = {'percentage': [], 'completed': []}
+
+        for program in self._program_requirement_cache:  # For every program
+            shortened = self._program_requirement_cache[program]
+
+            percentage = (shortened.have / shortened.need if shortened.need != 0 else 1) * 100  # Get percentage completed
+            to_finish = shortened.need - shortened.have  # Get amount of requirements needed to finish
+
+            values['percentage'].append([DEFAULT_PROGRAMS[program]['name'], percentage])
+            values['to_finish'].append([DEFAULT_PROGRAMS[program]['name'], to_finish])
+
+        values['percentage'].sort(key=lambda x: x[1])  # Sort from Highest -> Lowest
+        values['to_finish'] = sorted(values['to_finish'], reverse=False, key=lambda x: x[1])  # Sort from Least -> Most
+
+        return values
 
     def get_cgpa(self):
+        """
+        Return current grade point average from courses taken
+
+        :return: float
+        """
+
         total = 0
         credits = 0
 
         for course in self._taken_courses:
+            course = self._courses.course_cache[course]
             if course.type != 'Dropped':
                 # If the course wasn't dropped
 
@@ -189,48 +321,94 @@ class User:
             return total / credits
         return 0.0
 
-    def add_program(self, program_code):
-        if program_code not in self._programs:
-            program = deepcopy(DEFAULT_PROGRAMS[program_code])
-            self._programs[program_code] = program
-            self._programs[program_code]['requirements'] = convert_to_requirement(program['requirements'], self._courses)
+    # ============= SET METHODS ================== #
 
-            WORKSHEET.cell(column=3, row=USERS[self.username], value=str(list(self._programs.keys())))
-            WORKBOOK.save(FILE_NAME)
-        else:
-            print('Program already added!')
+    def set_mark(self, course_code, mark):
+        """
+        Set the mark of <course_code> to <mark>
 
-    def get_programs(self):
-        return self._programs
+        :param str course_code: Course to change
+        :param float mark: Mark of course
+        :return: None
+        """
 
-    def get_program(self, program_code):
-        return self._programs[program_code]
-
-    def remove_program(self, program_code):
         try:
-            self._programs.pop(program_code)
+            self._courses.change_mark(course_code, mark)
+
+            WORKSHEET.cell(column=2, row=USERS[self.username], value=str([[course_code, self._courses.get_type(course_code), self._courses.get_mark(course_code)] for course_code in self._taken_courses]))
+            WORKBOOK.save(FILE_NAME)
+            self._update_requirements()
+
         except KeyError:
-            print('Program does not exist!')
+            print('Course does not exist!')
 
-        WORKSHEET.cell(column=3, row=USERS[self.username], value=str(list(self._programs.keys())))
-        WORKBOOK.save(FILE_NAME)
+    def set_type(self, course_code, type):
+        """
+        Set the type of <course_code> to <type>
 
-    def get_program_requirements(self, program_code):
-        temp = self.convert_to_requirement(deepcopy(DEFAULT_PROGRAMS[program_code]['requirements']))
-        temp.have
-        return temp
+        :param str course_code: Course to change
+        :param str type: Type of course
+        :return: None
+        """
+
+        try:
+            if type == 'Completed' and self._taken_courses[course_code] < 50:
+                type = 'Failed'
+
+            self._courses.change_type(course_code, type)
+
+            WORKSHEET.cell(column=2, row=USERS[self.username], value=str([[course_code, self._courses.get_type(course_code), self._courses.get_mark(course_code)] for course_code in self._taken_courses]))
+            WORKBOOK.save(FILE_NAME)
+            self._update_requirements()
+
+        except KeyError:
+            print('Course does not exist!')
+
+    # ============= INITIAL OBJECT METHODS ================== #
 
     def initial_courses(self):
+        """
+        Add all pre-existing user programs
+
+        :return: None
+        """
         for course_info in literal_eval(WORKSHEET.cell(column=2, row=USERS[self.username]).value):
-            self.add_course(course_info[0])
+            self.add_course(course_info[0], initial=True)
             self.set_type(course_info[0], course_info[1])
             self.set_mark(course_info[0], course_info[2])
 
     def initial_programs(self):
+        """
+        Add all pre-existing user programs
+
+        :return: None
+        """
+
         for program_code in literal_eval(WORKSHEET.cell(column=3, row=USERS[self.username]).value):
             self.add_program(program_code)
 
+    def initial_program_cache(self):
+        """
+        Convert all the pre-existing user data into requirement objects
+
+        :return: None
+        """
+        for program in DEFAULT_PROGRAMS:
+            self._program_requirement_cache[program] = self.convert_to_requirement(deepcopy(DEFAULT_PROGRAMS[program]['requirements']))
+        self._update_requirements()
+
     def convert_to_requirement(self, requirements, exclusions=None, treatall=False, only_used=False, only_unused=False):
+        """
+        Converts data into an requirement object
+
+        :param list of str requirements: Courses that are needed for the program
+        :param list of str|None exclusions: Courses that cannot be used for this part of the requirement
+        :param bool treatall: If all courses are being looked through and marked as used
+        :param bool only_used: If only courses that have previously been used are being used
+        :param bool only_unused: If only courses that have previously not been used are being used
+        :return: Requirement
+        """
+
         modifier = requirements[0]
         max = requirements[-1]
         min = None
@@ -276,21 +454,40 @@ class User:
                     transformed_requirements.append(self._courses.course_cache[requirements[index]])
 
         else:
-            self._courses.add_course(requirements[1])
+            if requirements[1] not in self._courses.course_cache:
+                self._courses.add_course(requirements[1])
+
             transformed_requirements = self._courses.course_cache[requirements[1]]
 
         return Requirement(modifier, min, max, transformed_requirements, exclusions, treatall, only_used, only_unused)
 
 
 if __name__ == '__main__':
+    import time
 
+    start = time.time()
     while True:
+        start = time.time()
         usr = User(input('Username: ').lower(), input('Password: '))
 
         if usr.logged_in():
+            print('Startup Time:', time.time() - start, 'seconds')
             print('Logged in')
             break
         else:
+            print('Incorrect login!\n')
             del usr
 
-    print(usr.get_program_requirements('ASSPE1478'))
+    start = time.time()
+
+    print(usr.get_program_requirements('ASSPE0115'))
+
+    usr.remove_course('MAT223H1')
+
+    print('Update Time:', time.time() - start, 'seconds')
+
+    start = time.time()
+
+    print(usr.get_easiest())
+
+    print('Get easiest time:', time.time() - start, 'seconds')
